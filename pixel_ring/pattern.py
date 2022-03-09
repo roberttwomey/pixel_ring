f23b11a5b65c6c05d779259eb2bbd9e9f6f1c60f
Robert Twomey <robert.twomey@gmail.com>
2022-03-09 11:09:17 -0600

    added shell for smartenv lights pattern

--- pixel_ring/pattern.py
+++ pixel_ring/pattern.py
@@ -142,4 +142,73 @@ class GoogleHome(object):
     def off(self):
         self.show([0] * 4 * 12)
 
+        
+class SmartEnv(object):
+    def __init__(self, show):
+#         self.basis = [0] * 4 * 12
+#         self.basis[0 * 4 + 1] = 8
+#         self.basis[3 * 4 + 1] = 4
+#         self.basis[3 * 4 + 2] = 4
+#         self.basis[6 * 4 + 2] = 8
+#         self.basis[9 * 4 + 3] = 8
 
+#         self.pixels = self.basis
+
+        if not callable(show):
+            raise ValueError('show parameter is not callable')
+
+        # copied from Echo
+        self.pixels_number = 12
+        self.pixels = [0] * 4 * number
+        
+        self.show = show
+        self.stop = False
+
+    def wakeup(self, direction=0):
+        
+        pixels = [0, 0, 0, self.brightness] * self.pixels_number
+
+        self.show(pixels)
+
+    def listen(self):
+        pixels = self.pixels
+        for i in range(1, 25):
+            self.show([(v * i / 24) for v in pixels])
+            time.sleep(0.01)
+
+    def think(self):
+        pixels = self.pixels
+
+        while not self.stop:
+            pixels = pixels[-4:] + pixels[:-4]
+            self.show(pixels)
+            time.sleep(0.2)
+
+        t = 0.1
+        for i in range(0, 5):
+            pixels = pixels[-4:] + pixels[:-4]
+            self.show([(v * (4 - i) / 4) for v in pixels])
+            time.sleep(t)
+            t /= 2
+
+        self.pixels = pixels
+
+    def speak(self):
+        pixels = self.pixels
+        step = 1
+        brightness = 5
+        while not self.stop:
+            self.show([(v * brightness / 24) for v in pixels])
+            time.sleep(0.02)
+
+            if brightness <= 5:
+                step = 1
+                time.sleep(0.4)
+            elif brightness >= 24:
+                step = -1
+                time.sleep(0.4)
+
+            brightness += step
+
+    def off(self):
+        self.show([0] * 4 * 12)
